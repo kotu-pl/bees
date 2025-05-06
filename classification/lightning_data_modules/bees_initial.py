3f7ee3b3e512e7302d2161bdb867aded2be54188
Marcin Kot <marcin@kotu.pl>
2025-05-06 17:34:05 +0200

    Add Hydra-based file structure for future  experiments

---
+++ classification/lightning_data_modules/bees_initial.py
@@ -0,0 +1,56 @@
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split
+from torchvision.transforms import Resize, Compose, ToTensor, Normalize
+
+
+import gdown
+import zipfile
+import os.path as osp
+
+class BeesDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size, data_dir: str = '/content/bees_lighting/dataset'):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.image_size = (150, 300)
+        self.imagenet_transform = Compose([ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        self.num_classes = 5
+        self.zip_name = '/content/bees_dataset_300.zip'
+
+    def prepare_data(self):
+        if not osp.isfile(self.zip_name):
+            gdown.download('https://drive.google.com/uc?id=1S7xrUA1npvJqhsi0qqlyzubeStmp32Hi', output=self.zip_name, quiet=False)
+
+        if not osp.isdir(self.data_dir):
+            with zipfile.ZipFile(self.zip_name, 'r') as zip_ref:
+                zip_ref.extractall(self.data_dir)
+
+    def setup(self, stage=None):
+        full_dataset = ImageFolder(self.data_dir, transform=self.imagenet_transform)
+
+        train_dataset_size = int(len(full_dataset) * 0.7)
+        val_dataset_size = int(len(full_dataset) * 0.2)
+        test_dataset_size = len(full_dataset) - train_dataset_size - val_dataset_size
+
+        train_dataset, val_dataset, test_dataset = random_split(
+            full_dataset, [train_dataset_size, val_dataset_size, test_dataset_size]
+        )
+
+        if stage == 'fit' or stage is None:
+          self.train_dataset = train_dataset
+          self.val_dataset = val_dataset
+
+        if stage == 'test' or stage is None:
+          self.test_dataset = test_dataset
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import logging
+import hydra
+from hydra.utils import instantiate
+from hydra.utils import get_original_cwd, to_absolute_path
+
+from omegaconf import DictConfig, OmegaConf
+import pytorch_lightning as pl
+
+import torch
+# lightning related imports
+import pytorch_lightning as pl
+#from pytorch_lightning.metrics.functional import accuracy
+from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+    
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    # debugowanie
+    # print(OmegaConf.to_yaml(cfg))
+
+    data_module = instantiate(cfg.data)
+    data_module.prepare_data()
+    data_module.setup()
+
+    # Init our model
+    model = instantiate(cfg.network)
+
+    # Initialize trainer
+    trainer = instantiate(cfg.trainer)
+
+    # Train the model
+    trainer.fit(model, data_module)
+
+    # Evaluate the model on the held out test set ⚡⚡
+    trainer.test(model=model, datamodule=data_module)
+
+if __name__ == "__main__":
+    main()

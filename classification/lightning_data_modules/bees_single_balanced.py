69c47ec8b452b33395293be0e6c1057e8832805b
Marcin Kot <marcin@kotu.pl>
2025-05-14 19:44:20 +0200

    Add balanced set

--- classification/lightning_data_modules/bees_single_balanced.py
+++ classification/lightning_data_modules/bees_single_balanced.py
@@ -1,10 +1,10 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 import pytorch_lightning as pl
 from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
 from torchvision.transforms import Resize, Compose, ToTensor, Normalize
-
+from sklearn.model_selection import StratifiedShuffleSplit
 
 import gdown
 import zipfile
@@ -42,20 +42,31 @@ class BeesSingleBalancedDataModule(pl.LightningDataModule):
     def setup(self, stage=None):
         full_dataset = ImageFolder(self.data_dir, transform=self.imagenet_transform)
 
-        train_dataset_size = int(len(full_dataset) * 0.7)
-        val_dataset_size = int(len(full_dataset) * 0.2)
-        test_dataset_size = len(full_dataset) - train_dataset_size - val_dataset_size
+        # losowy podział, z równowagą, na train i temp
+        indices = list(range(len(full_dataset)))
+        labels  = [y for _, y in full_dataset.imgs]
 
-        train_dataset, val_dataset, test_dataset = random_split(
-            full_dataset, [train_dataset_size, val_dataset_size, test_dataset_size]
-        )
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=10)
+        train_idx, tmp_idx = next(sss.split(indices, labels))
 
-        if stage == 'fit' or stage is None:
-          self.train_dataset = train_dataset
-          self.val_dataset = val_dataset
+        # podział temp na val i test, 2:1
+        tmp_labels = [labels[i] for i in tmp_idx]
+        sss_val = StratifiedShuffleSplit(n_splits=1, test_size=1/3, random_state=42)
+        val_idx, test_idx = next(sss_val.split(tmp_idx, tmp_labels))
 
-        if stage == 'test' or stage is None:
-          self.test_dataset = test_dataset
+        self.train_dataset = Subset(full_dataset, train_idx)
+        self.val_dataset = Subset(full_dataset, [tmp_idx[i] for i in val_idx])
+        self.test_dataset = Subset(full_dataset, [tmp_idx[i] for i in test_idx])
+
+        # sampler z wagami
+        if stage in ("fit", None):
+            targets = [full_dataset.imgs[i][1] for i in train_idx]
+            class_counts = np.bincount(targets)
+            weights = 1.0 / class_counts
+            sample_weights = [weights[t] for t in targets]
+            self.train_sampler = WeightedRandomSampler(
+                sample_weights, num_samples=len(sample_weights), replacement=True
+            )
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)

b9fb6a6d414ad9560325239d022a638841521359
Marcin Kot <marcin@kotu.pl>
2025-06-07 16:28:12 +0200

    Conver BeesMultipleBalancedDataModule to multilabel

--- classification/lightning_data_modules/bees_multiple_balanced.py
+++ classification/lightning_data_modules/bees_multiple_balanced.py
@@ -6,6 +6,7 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
 from torchvision.transforms import RandomResizedCrop, RandomHorizontalFlip, RandomRotation, ColorJitter, Resize, Compose, ToTensor, Normalize
 from sklearn.model_selection import StratifiedShuffleSplit
+from torchvision.datasets.folder import default_loader
 import numpy as np
 
 import gdown
@@ -16,6 +17,22 @@ from .transforms_utils import ResizePad224
 
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD  = [0.229, 0.224, 0.225]
+
+class MultiLabelDataset(Dataset):
+    def __init__(self, samples, labels, transform=None):
+        self.samples = samples
+        self.labels = labels
+        self.transform = transform
+        self.loader = default_loader
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img = self.loader(self.samples[idx])
+        if self.transform:
+            img = self.transform(img)
+        return img, torch.tensor(self.labels[idx]).float()
 
 class BeesMultipleBalancedDataModule(pl.LightningDataModule):
     def __init__(self, batch_size, num_workers, data_dir: str = '', zip_path: str = '',  resize_pad_224: bool = False, augmentation: bool = False):
@@ -59,23 +76,37 @@ class BeesMultipleBalancedDataModule(pl.LightningDataModule):
                 zip_ref.extractall(self.data_dir)
 
     def setup(self, stage=None):
-        full_dataset = ImageFolder(self.data_dir)
+        img_paths, labels = self.load_multilabel_data()
 
         # losowy podział, z równowagą, na train i temp
-        indices = list(range(len(full_dataset)))
-        labels  = [y for _, y in full_dataset.imgs]
+        indices = list(range(len(img_paths)))
+        strat_labels = [tuple(label) for label in labels]
 
         sss = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=10)
-        train_idx, tmp_idx = next(sss.split(indices, labels))
+        train_idx, tmp_idx = next(sss.split(indices, strat_labels))
 
         # podział temp na val i test, 2:1
-        tmp_labels = [labels[i] for i in tmp_idx]
+        tmp_labels = [strat_labels[i] for i in tmp_idx]
         sss_val = StratifiedShuffleSplit(n_splits=1, test_size=1/3, random_state=42)
         val_idx, test_idx = next(sss_val.split(tmp_idx, tmp_labels))
 
-        self.train_dataset = Subset(full_dataset, train_idx)
-        self.val_dataset = Subset(full_dataset, [tmp_idx[i] for i in val_idx])
-        self.test_dataset = Subset(full_dataset, [tmp_idx[i] for i in test_idx])
+        self.train_dataset = MultiLabelDataset(
+            [img_paths[i] for i in train_idx],
+            [labels[i] for i in train_idx],
+            transform=self.train_transform
+        )
+
+        self.val_dataset = MultiLabelDataset(
+            [img_paths[tmp_idx[i]] for i in val_idx],
+            [labels[tmp_idx[i]] for i in val_idx],
+            transform=self.eval_transform
+        )
+
+        self.test_dataset = MultiLabelDataset(
+            [img_paths[tmp_idx[i]] for i in test_idx],
+            [labels[tmp_idx[i]] for i in test_idx],
+            transform=self.eval_transform
+        )
 
         self.train_dataset.dataset.transform = self.train_transform
         self.val_dataset.dataset.transform = self.eval_transform
@@ -83,12 +114,11 @@ class BeesMultipleBalancedDataModule(pl.LightningDataModule):
 
         # sampler z wagami
         if stage in ("fit", None):
-            targets = [full_dataset.imgs[i][1] for i in train_idx]
-            class_counts = np.bincount(targets)
-            weights = 1.0 / class_counts
-            sample_weights = [weights[t] for t in targets]
+            class_counts = np.sum([labels[i] for i in train_idx], axis=0)
+            weights = 1.0 / (class_counts + 1e-6)
+            sample_weights = np.array([np.sum(labels[i] * weights) for i in train_idx])
             self.train_sampler = WeightedRandomSampler(
-                sample_weights, num_samples=len(sample_weights), replacement=True
+                sample_weights, len(sample_weights), replacement=True
             )
 
     def train_dataloader(self):
@@ -115,25 +145,18 @@ class BeesMultipleBalancedDataModule(pl.LightningDataModule):
             num_workers=self.num_workers
         )
 
-    def add_bee_output(example):
-        outputs = example['output']
-        sum_other = (
-            outputs['cooling_output'] +
-            outputs['pollen_output'] +
-            outputs['varroa_output'] +
-            outputs['wasps_output']
-        )
-        bee = tf.cast(tf.equal(sum_other, 0.0), tf.float32)
-        outputs['bee_output'] = bee
-        example['output'] = outputs
-        return example
+    def load_multilabel_data(self):
+        class_names = sorted(os.listdir(self.data_dir))
+        class_to_idx = {cls: i for i, cls in enumerate(class_names)}
 
-    def rename_labels(example):
-        outputs = example['output']
-        renamed = {}
-        for key, value in outputs.items():
-            new_key = key.replace('_output', '')
-            renamed[new_key] = value
-        example['output'] = renamed
-        return example
+        img_to_labels = {}
+        for cls in class_names:
+            folder = os.path.join(self.data_dir, cls)
+            for img_path in glob(os.path.join(folder, '*')):
+                if img_path not in img_to_labels:
+                    img_to_labels[img_path] = np.zeros(len(class_names), dtype=float)
+                img_to_labels[img_path][class_to_idx[cls]] = 1
 
+        img_paths = list(img_to_labels.keys())
+        labels    = list(img_to_labels.values())
+        return img_paths, labels
